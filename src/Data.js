43ad992ec4d1d454e6bb59b85c94b0adcdd2c64b
Shashank <mv.shashank123@gmail.com>
2020-10-22 12:59:11 +0530

    Added Bar and Bump

---
+++ src/Data.js
@@ -0,0 +1,255 @@
+export const BarData=[
+    {
+      "country": "AD",
+      "hot dog": 124,
+      "hot dogColor": "hsl(160, 70%, 50%)",
+      "burger": 163,
+      "burgerColor": "hsl(102, 70%, 50%)",
+      "sandwich": 181,
+      "sandwichColor": "hsl(255, 70%, 50%)",
+      "kebab": 194,
+      "kebabColor": "hsl(40, 70%, 50%)",
+      "fries": 156,
+      "friesColor": "hsl(125, 70%, 50%)",
+      "donut": 140,
+      "donutColor": "hsl(173, 70%, 50%)"
+    },
+    {
+      "country": "AE",
+      "hot dog": 15,
+      "hot dogColor": "hsl(259, 70%, 50%)",
+      "burger": 73,
+      "burgerColor": "hsl(146, 70%, 50%)",
+      "sandwich": 1,
+      "sandwichColor": "hsl(141, 70%, 50%)",
+      "kebab": 33,
+      "kebabColor": "hsl(43, 70%, 50%)",
+      "fries": 113,
+      "friesColor": "hsl(232, 70%, 50%)",
+      "donut": 128,
+      "donutColor": "hsl(77, 70%, 50%)"
+    },
+    {
+      "country": "AF",
+      "hot dog": 194,
+      "hot dogColor": "hsl(331, 70%, 50%)",
+      "burger": 122,
+      "burgerColor": "hsl(311, 70%, 50%)",
+      "sandwich": 77,
+      "sandwichColor": "hsl(154, 70%, 50%)",
+      "kebab": 26,
+      "kebabColor": "hsl(53, 70%, 50%)",
+      "fries": 138,
+      "friesColor": "hsl(62, 70%, 50%)",
+      "donut": 110,
+      "donutColor": "hsl(36, 70%, 50%)"
+    },
+    {
+      "country": "AG",
+      "hot dog": 122,
+      "hot dogColor": "hsl(218, 70%, 50%)",
+      "burger": 85,
+      "burgerColor": "hsl(74, 70%, 50%)",
+      "sandwich": 7,
+      "sandwichColor": "hsl(182, 70%, 50%)",
+      "kebab": 156,
+      "kebabColor": "hsl(114, 70%, 50%)",
+      "fries": 61,
+      "friesColor": "hsl(36, 70%, 50%)",
+      "donut": 170,
+      "donutColor": "hsl(160, 70%, 50%)"
+    },
+    {
+      "country": "AI",
+      "hot dog": 115,
+      "hot dogColor": "hsl(113, 70%, 50%)",
+      "burger": 20,
+      "burgerColor": "hsl(237, 70%, 50%)",
+      "sandwich": 104,
+      "sandwichColor": "hsl(319, 70%, 50%)",
+      "kebab": 158,
+      "kebabColor": "hsl(39, 70%, 50%)",
+      "fries": 80,
+      "friesColor": "hsl(288, 70%, 50%)",
+      "donut": 9,
+      "donutColor": "hsl(52, 70%, 50%)"
+    },
+    {
+      "country": "AL",
+      "hot dog": 83,
+      "hot dogColor": "hsl(45, 70%, 50%)",
+      "burger": 75,
+      "burgerColor": "hsl(220, 70%, 50%)",
+      "sandwich": 174,
+      "sandwichColor": "hsl(99, 70%, 50%)",
+      "kebab": 131,
+      "kebabColor": "hsl(110, 70%, 50%)",
+      "fries": 138,
+      "friesColor": "hsl(58, 70%, 50%)",
+      "donut": 2,
+      "donutColor": "hsl(236, 70%, 50%)"
+    },
+    {
+      "country": "AM",
+      "hot dog": 148,
+      "hot dogColor": "hsl(299, 70%, 50%)",
+      "burger": 134,
+      "burgerColor": "hsl(284, 70%, 50%)",
+      "sandwich": 36,
+      "sandwichColor": "hsl(283, 70%, 50%)",
+      "kebab": 30,
+      "kebabColor": "hsl(59, 70%, 50%)",
+      "fries": 199,
+      "friesColor": "hsl(325, 70%, 50%)",
+      "donut": 81,
+      "donutColor": "hsl(205, 70%, 50%)"
+    }
+  ]
+
+export const BumpData=[
+    {
+      "id": "JavaScript",
+      "data": [
+        {
+          "x": 2000,
+          "y": 11
+        },
+        {
+          "x": 2001,
+          "y": 16
+        },
+        {
+          "x": 2002,
+          "y": 16
+        },
+        {
+          "x": 2003,
+          "y": 18
+        },
+        {
+          "x": 2004,
+          "y": 18
+        },
+        {
+          "x": 2005,
+          "y": 16
+        }
+      ]
+    },
+    {
+      "id": "ReasonML",
+      "data": [
+        {
+          "x": 2000,
+          "y": 25
+        },
+        {
+          "x": 2001,
+          "y": 23
+        },
+        {
+          "x": 2002,
+          "y": 15
+        },
+        {
+          "x": 2003,
+          "y": 16
+        },
+        {
+          "x": 2004,
+          "y": 15
+        },
+        {
+          "x": 2005,
+          "y": 27
+        }
+      ]
+    },
+    {
+      "id": "TypeScript",
+      "data": [
+        {
+          "x": 2000,
+          "y": 23
+        },
+        {
+          "x": 2001,
+          "y": 27
+        },
+        {
+          "x": 2002,
+          "y": 15
+        },
+        {
+          "x": 2003,
+          "y": 12
+        },
+        {
+          "x": 2004,
+          "y": 10
+        },
+        {
+          "x": 2005,
+          "y": 26
+        }
+      ]
+    },
+    {
+      "id": "Elm",
+      "data": [
+        {
+          "x": 2000,
+          "y": 11
+        },
+        {
+          "x": 2001,
+          "y": 19
+        },
+        {
+          "x": 2002,
+          "y": 20
+        },
+        {
+          "x": 2003,
+          "y": 12
+        },
+        {
+          "x": 2004,
+          "y": 18
+        },
+        {
+          "x": 2005,
+          "y": 22
+        }
+      ]
+    },
+    {
+      "id": "CoffeeScript",
+      "data": [
+        {
+          "x": 2000,
+          "y": 17
+        },
+        {
+          "x": 2001,
+          "y": 22
+        },
+        {
+          "x": 2002,
+          "y": 25
+        },
+        {
+          "x": 2003,
+          "y": 24
+        },
+        {
+          "x": 2004,
+          "y": 27
+        },
+        {
+          "x": 2005,
+          "y": 11
+        }
+      ]
+    }
+  ]
